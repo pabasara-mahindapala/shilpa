9b92f8230727c71eb5f2996ead4681145cce4d53
pabasara-mahindapala <pabasaramahindapala@gmail.com>
2020-01-13 16:05:18 +0530

    Added Enrollments

---
+++ EE5207.Project.EntityFramework/Migrations/202001131031216_Added-Enrollments.cs
@@ -0,0 +1,16 @@
+namespace EE5207.Project.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddedEnrollments : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
